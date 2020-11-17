fe9201c16696ebff752c0870224564d1db8b845e
lmnoo <jerkkuq@users.noreply.github.com>
2020-11-17 13:11:28 +0200

    adding some styles and somewhat working scaling for mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,27 +57,6 @@ const App = () => {
 					</div>
 				)}
 			</div>
-			{/* <div>
-				<h1>Add New Game</h1>
-				<form method="POST" action="http://localhost:8080/add-game">
-					<div>
-						<label>Name</label>
-						<input type="text" name="name" required />
-					</div>
-					<div>
-						<label>Image</label>
-						<input type="text" name="image" required />
-					</div>
-					<div>
-						<label>Description</label>
-						<input type="text" name="description" required />
-					</div>
-
-					<div>
-						<button type="submit">Add Game</button>
-					</div>
-				</form>
-			</div> */}
 		</Fragment>
 	);
 };
